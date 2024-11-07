3532bee8b21a3a1ec4ba3edd2039a8edf5fdcd13
Victor-Drt <victorduarte778@gmail.com>
2024-11-07 15:02:02 -0400

    fix: layout pagina de dispositivos

--- lib/pages/antigo_comparativo.dart
+++ lib/pages/antigo_comparativo.dart
@@ -10,6 +10,53 @@ class DeprecatedPage extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: 
+                      //         Container(
+                      //   margin: const EdgeInsets.fromLTRB(6, 16, 6, 32),
+                      //   width: MediaQuery.sizeOf(context).width * 0.8,
+                      //   padding: const EdgeInsets.all(6.0),
+                      //   decoration: const BoxDecoration(
+                      //     color: Color.fromRGBO(239, 249, 244, 1.0),
+                      //     borderRadius: BorderRadius.all(Radius.circular(20)),
+                      //   ),
+                      //   child: DropdownButton<String>(
+                      //     isExpanded: true,
+                      //     alignment: AlignmentDirectional.center,
+                      //     style: estiloTextoDropdown,
+                      //     hint: Text(
+                      //       "Escolha o Bloco",
+                      //       textAlign: TextAlign.center,
+                      //       style: estiloTextoDropdown,
+                      //     ),
+                      //     value: dropdownValue,
+                      //     icon: const Icon(Icons.filter_alt_outlined),
+                      //     onChanged: (String? value) {
+                      //       setState(() {
+                      //         dropdownValue = value;
+                      //         if (dropdownValue != null) {
+                      //           // Filtra dispositivos pelo ID do ambiente
+                      //           itensDispositivoFiltrado = itensDispositivo
+                      //               .where((e) => e.ambienteId == dropdownValue)
+                      //               .toList();
+                      //         }
+                      //       });
+                      //     },
+                      //     items: blocos
+                      //         .map<DropdownMenuItem<String>>((Bloco value) {
+                      //       return DropdownMenuItem<String>(
+                      //         alignment: Alignment.center,
+                      //         value: value.id.toString(),
+                      //         child: Text(
+                      //           style: TextStyle(
+                      //               color: Colors.black,
+                      //               fontSize: 16,
+                      //               fontWeight: FontWeight.bold),
+                      //           value.nome.toString(),
+                      //         ),
+                      //       );
+                      //     }).toList(),
+                      //   ),
+                      // ),
+
           // BarChartSample(),
           // LineChartSample()
           PieChartSample()
